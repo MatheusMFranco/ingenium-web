48f300729a49cc4f8104d3e87b1b399a298a54c4
Matheus Franco <Matheus Franco>
2016-05-07 00:10:07 -0300

    Remoção de arquivos para nova arquitetura

--- ingenium-prime/src/main/webapp/resources/js/main.js
+++
@@ -1,5 +0,0 @@
-/* 
-	Autor: Matheus Franco
-	Criação: 04/2016 
-	Arquivo: Main.css
-*/
--- ingenium-prime/target/classes/META-INF/sql/dados-iniciais.sql
+++
@@ -1,1 +0,0 @@
-insert into Usuario (id, conta_usuario, nome_usuario, email_usuario, tipo, senha_usuario) values (1, 'matheusfranco', 'Matheus M Franco', 'cajondefox@gmail.com', 'ADM', 'PaoDeQueijo');
